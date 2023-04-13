35896029d4c5c16e4a45262e5a579d1d0bbc8aa3
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-13 23:07:53 +0300

    Update MRO.py

--- MRO.py
+++ MRO.py
@@ -36,33 +36,43 @@ Yes
 Yes
 No'''
 
-dict_main = {}
+classes = {}
 
-for i in range(int(input())):
-    string = input().split(':')
-    if len(string) == 1:
-        dict_main[string[0].strip()] = set()
-    else:
-        dict_main[string[0].strip()] = {x.strip() for x in string[1].split()}
+def add_class(classes, class_name, parents):
+    if class_name not in classes:
+        classes[class_name] = []
+    classes[class_name].extend(parents)
+    for parent in parents:
+        if parent not in classes:
+            classes[parent] = []
 
+def found_path(classes, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if start not in classes:
+        return None
+    for node in classes[start]:
+        if node not in path:
+            newpath = found_path(classes, node, end, path)
+            if newpath: return newpath
+    return None
 
-def search(parent, child):
-    if parent == child:
-        return 1
-    elif parent in dict_main[child]:
-        return 1
-    else:
-        for p in dict_main[child]:
-            if search(p, child):
-                return 1
+def answer(classes, parent, child):
+    if not(parent or child) in classes or not found_path(classes, child, parent):
+        return 'No'
+    return 'Yes'
 
+n = int(input())
+for _ in range(n):
+    class_description = input().split()
+    class_name = class_description[0]
+    class_parents = class_description[2:]
+    add_class(classes, class_name, class_parents)
 
-questions = []
-for i in range(int(input())):
-    questions.append(input().split())
-
-for question in questions:
-    if search(question[0], question[1]):
-        print('Yes')
-    else:
-        print('No')
+q = int(input())
+for _ in range(q):
+    question = input().split()
+    parent = question[0]
+    child = question[1]
+    print(answer(classes, parent, child))
