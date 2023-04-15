ba71c68f68fc130886cfae07b35ff0af1dfaf1b5
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-15 14:56:28 +0300

    Create class_doubleElListIter_1.py

---
+++ class_doubleElListIter_1.py
@@ -0,0 +1,22 @@
+'''выводим парами элементы списка'''
+
+class DoubleElementListIterator:
+    def __init__(self, lst):
+        self.lst = lst
+        self.i = 0
+
+    def __next__(self):
+        if self.i < len(self.lst):
+            self.i += 2
+            return self.lst(self.i - 2), self.lst(self.i - 1)
+        else:
+            raise StopIteration
+
+
+class MyList(list):
+    def __iter__(self):
+        return DoubleElementListIterator(self)
+
+
+for pair in MyList([1, 2, 4, 5, 6, 7]):
+    print(pair)
