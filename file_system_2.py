5a36715bdb01c226905149dd43d99cb803605270
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-18 00:39:33 +0300

    Update file_system_2.py

--- file_system_2.py
+++ file_system_2.py
@@ -23,3 +23,12 @@ with open('out.txt', 'w') as out:
     for path in result_lst:
         path = path.replace('\\', '/')
         out.write(path + '\n')
+
+#вариант два (короткий)
+import os
+
+result = [cur_dir for cur_dir, dirs, files in os.walk("main") if any((fl.endswith(".py")
+    for fl in files))]
+
+with open("py_dirs.txt", "w") as w:
+    w.write("\n".join(sorted(result)))
