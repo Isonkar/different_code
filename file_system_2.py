527db0f2df9b9cd6460017a027db84ef8dfc142a
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-18 00:37:50 +0300

    Create file_system_2.py

---
+++ file_system_2.py
@@ -0,0 +1,25 @@
+'''Вам дана в архиве (ссылка) файловая структура, состоящая из директорий и файлов.
+
+Вам необходимо распаковать этот архив, и затем найти в данной в файловой структуре все директории, в которых есть хотя бы один файл с расширением ".py". 
+Ответом на данную задачу будет являться файл со списком таких директорий, отсортированных в лексикографическом порядке.
+Для лучшего понимания формата задачи, ознакомьтесь с примером.
+'''
+
+import os
+import os.path
+
+
+result_lst = []
+with open('out.txt', 'w') as out:
+
+    for current_dir, dirs, files in os.walk('main'):
+        for file in files:
+            if file.endswith('.py'):
+                # if current_dir not in result_lst:
+                result_lst.append(current_dir)
+                break
+
+    result_lst.sort()
+    for path in result_lst:
+        path = path.replace('\\', '/')
+        out.write(path + '\n')
