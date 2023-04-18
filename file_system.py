2ca56f14b48bd0e83611fa0575f512a6e1f50644
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-18 22:32:07 +0300

    Update file_system.py

--- file_system.py
+++ file_system.py
@@ -9,18 +9,18 @@ print(os.getcwd()) #какая папка является текущей
 print(os.listdir('.idea')) #перечисляет все, что содержится в текущей папке
 
 print(os.path.exists('general.py'))
-print(os.path.exists('new_general.py'))
+print(os.path.exists('new_general.py')) #возвращает булевое значение "есть ли этот файл"
 print(os.path.exists('venv'))
 
-print(os.path.isfile('general.py'))
-print(os.path.isdir('venv'))
+print(os.path.isfile('general.py')) # проверка "это файл"
+print(os.path.isdir('venv')) # проверка "это папка"
 
-print(os.path.abspath('general.py'))
+print(os.path.abspath('general.py')) # показывает абсолютный путь к файлу
 
-# os.chdir('yaprakti')
+# os.chdir('yaprakti') # смена директории
 # print(os.getcwd())
 
-shutil.copy('dataout.txt', 'dataout2.txt')
+shutil.copy('dataout.txt', 'dataout2.txt') #копирует первый файл во второй (т.е. создает копию первого)
 
-for current_dir, dirs, files in os.walk('.'):
+for current_dir, dirs, files in os.walk('.'): #проходит все папки и подпапки и выводит current_dir, dirs, files
     print(current_dir, dirs, files)
