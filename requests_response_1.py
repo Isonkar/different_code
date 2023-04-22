6b1b58538e102225abe79bfb1232eb0e8c35ab65
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-22 14:35:38 +0300

    Create requests_response_1.py

---
+++ requests_response_1.py
@@ -0,0 +1,20 @@
+''' к уроку из курса "Python основы и преминения" раздел "немного про интернет"
+в данном примере обращаемся к сайту с документацией пайона 3.5 и сохраняем иконку в файл на ПК, также пробуем другие простые операции
+'''
+
+import requests
+
+res = requests.get('https://docs.python.org/3.5/_static/py.png')
+print(res.status_code)
+print(res.headers['Content-Type'])
+
+with open('python.png', 'wb') as f:
+    f.write(res.content)
+
+# print(res.content)
+#print(res.text)
+
+# for k, v in res.headers.items():
+#     print(f'{k}: {v}')
+
+
